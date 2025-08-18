95dea7ab82bb9643c002f1c406ee034e6337a8b6
Tristan <tristanbs2015@gmail.com>
2025-08-18 19:22:05 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.cab302.globetrotter {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.cab302.globetrotter to javafx.fxml;
+    exports com.cab302.globetrotter;
+}
